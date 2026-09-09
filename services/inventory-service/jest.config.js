4ebe7d891987ce95d36cf317711b5d530b66b8e2
Pruthwiraj <pruthwirajnayak08@gmail.com>
2026-09-09 10:58:12 +0530

    test(inventory): add inventory and reservation integration coverage

---
+++ services/inventory-service/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.test.ts"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
+  testTimeout: 30000,
+};
